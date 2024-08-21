f0586f97e0b8a76db129c2d32997e1c1be045e52
laumcs21 <lauram.cardenass@uqvirtual.edu.co>
2024-08-20 23:05:12 -0500

    Se agrega print

--- src/main/java/www/uniquindio/edu/poo/CrearCitaView.java
+++ src/main/java/www/uniquindio/edu/poo/CrearCitaView.java
@@ -29,6 +29,8 @@ public class CrearCitaView {
         fisioClinic = FisioClinic.getInstancia();
         this.usuarioLogueado = usuarioLogueado;
 
+        System.out.println();
+
         tipoTerapiaComboBox = new ComboBox<>();
         tipoTerapiaComboBox.getItems().addAll(TipoTerapia.values());
         tipoTerapiaComboBox.setPromptText("Seleccione el tipo de terapia");
